be95fd6ee44d4be3a580d6ae4343a70721a6dcfc
Digriz <fiveadvert@gmail.com>
2024-08-10 20:34:29 -0500

    Добавлено решение домашнего задания 6

---
+++ js_homework6.js
@@ -0,0 +1,71 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
+
+function findValueByKey(companyName, obj) {
+    if (obj.name === companyName) {
+        return obj;
+    }
+    
+    if (obj.clients) {
+        for (let client of obj.clients) {
+            let result = findValueByKey(companyName, client);
+            if (result) {
+                return result;
+            }
+        }
+    }
+    
+    if (obj.partners) {
+        for (let partner of obj.partners) {
+            let result = findValueByKey(companyName, partner);
+            if (result) {
+                return result;
+            }
+        }
+    }
+    
+    return null;
+}
+
+// Пример использования
+console.log(findValueByKey('Клієнт 1.2.3', company));
